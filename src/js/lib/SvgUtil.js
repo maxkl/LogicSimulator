bf00464461b2cc6b77ad4050a1df1de2bb629e31
maxkl <max@maxkl.de>
2017-03-28 21:48:43 +0200

    Add method for creating SVGs

--- src/js/lib/SvgUtil.js
+++ src/js/lib/SvgUtil.js
@@ -6,11 +6,16 @@
 define(function () {
 	var NS = 'http://www.w3.org/2000/svg';
 
+	function createSvg() {
+		return document.createElementNS(NS, 'svg');
+	}
+
 	function createSvgElement(name) {
 		return document.createElementNS(NS, name);
 	}
 
 	return {
+		createSvg: createSvg,
 		createElement: createSvgElement
 	};
 });

514b37b5370ffa59a195118afdcf43eba80ff5c4
maxkl <max@maxkl.de>
2017-03-29 16:31:52 +0200

    Don't add components at startup

--- src/js/main.js
+++ src/js/main.js
@@ -4,24 +4,7 @@
  */
 
 define([
-	'lib/SvgUtil',
-	'App',
-	'editor/components/AndComponent',
-	'editor/components/ClockComponent',
-	'editor/components/ConstComponent',
-	'editor/components/DFlipFlopComponent',
-	'editor/components/FullAdderComponent',
-	'editor/components/HalfAdderComponent',
-	'editor/components/NotComponent',
-	'editor/components/OrComponent'
-], function (SvgUtil, App, AndComponent, ClockComponent, ConstComponent, DFlipFlopComponent, FullAdderComponent, HalfAdderComponent, NotComponent, OrComponent) {
+	'App'
+], function (App) {
 	var app = new App();
-	app.editor.addComponent(new AndComponent(), 10, 10);
-	app.editor.addComponent(new ClockComponent(), 20, 10);
-	app.editor.addComponent(new ConstComponent(true), 10, 30);
-	app.editor.addComponent(new DFlipFlopComponent(), 20, 30);
-	app.editor.addComponent(new FullAdderComponent(), 10, 50);
-	app.editor.addComponent(new HalfAdderComponent(), 20, 50);
-	app.editor.addComponent(new NotComponent(), 10, 70);
-	app.editor.addComponent(new OrComponent(), 20, 70);
 });

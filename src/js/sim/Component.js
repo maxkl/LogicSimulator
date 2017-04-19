108bc96003521d2150dd439e286546c69a6edba6
maxkl <max@maxkl.de>
2017-04-19 20:44:07 +0200

    Remove unused code in `sim/Component.js`

--- src/js/sim/Component.js
+++ src/js/sim/Component.js
@@ -4,18 +4,9 @@
  */
 
 define(function () {
-	function Component(args) {
-		this.args = Array.prototype.slice.call(args);
-
+	function Component() {
 		this.userData = null;
 	}
-
-	Component.prototype.construct = function () {
-		var ctor = this.constructor;
-		var args = [ null ].concat(this.args);
-		var boundCtor = ctor.bind.apply(ctor, args);
-		return new boundCtor();
-	};
 
 	return Component;
 });
